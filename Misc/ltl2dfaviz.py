f484c6517e268fe4a0ec59cf3c7c340645101ca1
Mudit Verma <mudit.verma2014@gmail.com>
2021-03-18 13:25:10 -0700

    ltl 2 graph nx

--- Misc/ltl2dfaviz.py
+++ Misc/ltl2dfaviz.py
@@ -1,6 +1,9 @@
 from ltlf2dfa.parser.pltlf import PLTLfParser
 from graphviz import Source
 
+import pygraphviz
+from networkx.drawing import nx_agraph
+
 
 FORMULA = "H(a -> Y b)"
 
@@ -17,12 +20,32 @@ dfa = formula.to_dfa()
 # dfa.replace("\n", "")
 
 
+def convertToGraph(dfa):
+	G = nx_agraph.from_agraph(pygraphviz.AGraph(dfa))
+
+	return G
+
+def view_graph(dfa):
+	s = Source(dfa, filename="dfa.gv", format="png")
+	s.view()
+
+
+view_graph(dfa)
 
 
 
-s = Source(dfa, filename="dfa.gv", format="png")
-s.view()
+G = convertToGraph(dfa)
 
+print (type(G))
 
+# to print nodes
+print (G.nodes)
+
+# to print edge labels
+print(G.edges(data=True))
 
 # use networkx to convert dfa dot to networkx graph so that it can be later modified. 
+
+
+
+
