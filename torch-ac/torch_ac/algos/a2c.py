0040073f75eeac4ab11a89263ca912a694ecc3ba
Naman Shah <npshah4@asu.edu>
2021-04-13 20:16:29 -0700

    a2c update

--- torch-ac/torch_ac/algos/a2c.py
+++ torch-ac/torch_ac/algos/a2c.py
@@ -9,11 +9,11 @@ class A2CAlgo(BaseAlgo):
 
     def __init__(self, envs, acmodel, device=None, num_frames_per_proc=None, discount=0.99, lr=0.01, gae_lambda=0.95,
                  entropy_coef=0.01, value_loss_coef=0.5, max_grad_norm=0.5, recurrence=4,
-                 rmsprop_alpha=0.99, rmsprop_eps=1e-8, preprocess_obss=None, reshape_reward=None):
+                 rmsprop_alpha=0.99, rmsprop_eps=1e-8, preprocess_obss=None, reshape_reward=None, dfa_list=None):
         num_frames_per_proc = num_frames_per_proc or 8
 
         super().__init__(envs, acmodel, device, num_frames_per_proc, discount, lr, gae_lambda, entropy_coef,
-                         value_loss_coef, max_grad_norm, recurrence, preprocess_obss, reshape_reward)
+                         value_loss_coef, max_grad_norm, recurrence, preprocess_obss, reshape_reward,dfa_list)
 
         self.optimizer = torch.optim.RMSprop(self.acmodel.parameters(), lr,
                                              alpha=rmsprop_alpha, eps=rmsprop_eps)
--- torch_rl/scripts/train.py
+++ torch_rl/scripts/train.py
@@ -15,7 +15,7 @@ import util
 
 parser = argparse.ArgumentParser()
 
-## General parameters
+## General parameters   #default
 parser.add_argument("--algo", required=True,
                     help="algorithm to use: a2c | ppo (REQUIRED)")
 parser.add_argument("--env", required=True,
@@ -28,17 +28,46 @@ parser.add_argument("--log-interval", type=int, default=1,
                     help="number of updates between two logs (default: 1)")
 parser.add_argument("--save-interval", type=int, default=1,
                     help="number of updates between two saves (default: 10, 0 means no saving)")
-parser.add_argument("--procs", type=int, default=2,
+parser.add_argument("--procs", type=int, default=16,
                     help="number of processes (default: 16)")
 parser.add_argument("--frames", type=int, default=10**7,
                     help="number of frames of training (default: 1e7)")
 
-## Parameters for main algorithm
+## Parameters for main algorithm #default
+# parser.add_argument("--epochs", type=int, default=4,
+#                     help="number of epochs for PPO (default: 4)")
+# parser.add_argument("--batch-size", type=int, default=256,
+#                     help="batch size for PPO (default: 256)")
+# parser.add_argument("--frames-per-proc", type=int, default=None,
+#                     help="number of frames per process before update (default: 5 for A2C and 128 for PPO)")
+# parser.add_argument("--discount", type=float, default=0.99,
+#                     help="discount factor (default: 0.99)")
+# parser.add_argument("--lr", type=float, default=0.001,
+#                     help="learning rate (default: 0.001)")
+# parser.add_argument("--gae-lambda", type=float, default=0.95,
+#                     help="lambda coefficient in GAE formula (default: 0.95, 1 means no gae)")
+# parser.add_argument("--entropy-coef", type=float, default=0.01,
+#                     help="entropy term coefficient (default: 0.01)")
+# parser.add_argument("--value-loss-coef", type=float, default=0.5,
+#                     help="value loss term coefficient (default: 0.5)")
+# parser.add_argument("--max-grad-norm", type=float, default=0.5,
+#                     help="maximum norm of gradient (default: 0.5)")
+# parser.add_argument("--optim-eps", type=float, default=1e-8,
+#                     help="Adam and RMSprop optimizer epsilon (default: 1e-8)")
+# parser.add_argument("--optim-alpha", type=float, default=0.99,
+#                     help="RMSprop optimizer alpha (default: 0.99)")
+# parser.add_argument("--clip-eps", type=float, default=0.2,
+#                     help="clipping epsilon for PPO (default: 0.2)")
+# parser.add_argument("--recurrence", type=int, default=1,
+#                     help="number of time-steps gradient is backpropagated (default: 1). If > 1, a LSTM is added to the model to have memory.")
+# parser.add_argument("--text", action="store_true", default=False,
+#                     help="add a GRU to the model to handle text input")
+
 parser.add_argument("--epochs", type=int, default=4,
                     help="number of epochs for PPO (default: 4)")
 parser.add_argument("--batch-size", type=int, default=256,
                     help="batch size for PPO (default: 256)")
-parser.add_argument("--frames-per-proc", type=int, default=None,
+parser.add_argument("--frames-per-z", type=int, default=None,
                     help="number of frames per process before update (default: 5 for A2C and 128 for PPO)")
 parser.add_argument("--discount", type=float, default=0.99,
                     help="discount factor (default: 0.99)")
@@ -58,7 +87,7 @@ parser.add_argument("--optim-alpha", type=float, default=0.99,
                     help="RMSprop optimizer alpha (default: 0.99)")
 parser.add_argument("--clip-eps", type=float, default=0.2,
                     help="clipping epsilon for PPO (default: 0.2)")
-parser.add_argument("--recurrence", type=int, default=1,
+parser.add_argument("--recurrence", type=int, default=0,
                     help="number of time-steps gradient is backpropagated (default: 1). If > 1, a LSTM is added to the model to have memory.")
 parser.add_argument("--text", action="store_true", default=False,
                     help="add a GRU to the model to handle text input")
